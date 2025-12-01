8c760ad3a0ca01215ac5a81a393c26488fa9a8ec
theogaditya <theogadityahota@gmail.com>
2025-12-01 13:09:00 +0530

    updated prisma schema

---
+++ packages/user-be/prisma/migrations/20251201073542_updated_districs/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `state` on the `regional_workflows` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "regional_workflows" DROP COLUMN "state";
