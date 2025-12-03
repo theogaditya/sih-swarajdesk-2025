73d4d073983b8397e0e95ef4b4bb866d40dde75e
Abhas Behera <abhasbehera560@gmail.com>
2025-12-04 00:50:07 +0530

    added CI for admin-fe and minor fix

--- packages/user-fe/components/Navbar.tsx
+++ packages/user-fe/components/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className='fixed top-0 left-0 right-0 z-[99999999] bg-white/46 backdrop-blur-md'>
+    <nav className='fixed top-0 left-0 right-0 z-99999999 bg-white/46 backdrop-blur-md'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex items-center justify-between h-14'>
           {/* Logo */}
